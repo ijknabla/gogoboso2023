b38890f0801a7e51008996b1fa3140b382c67f8d
ijknabla <ijknabla@gmail.com>
2023-08-29 11:34:29 +0900

    Add bootstrap package

---
+++ bootstrap/__main__.py
@@ -0,0 +1,32 @@
+from asyncio import run
+from collections.abc import Callable, Coroutine
+from functools import wraps
+from typing import Any, ParamSpec, TypeVar
+
+import click
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+def run_decorator(f: Callable[P, Coroutine[Any, Any, T]]) -> Callable[P, T]:
+    @wraps(f)
+    def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
+        return run(f(*args, **kwargs))
+
+    return wrapped
+
+
+@click.group
+def main() -> None:
+    ...
+
+
+@main.command
+@run_decorator
+async def 市町村() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    main()
