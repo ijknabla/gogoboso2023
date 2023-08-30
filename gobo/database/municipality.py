eaf9ba52ae215c29ae1c787fa3d8aa865dbfafc6
ijknabla <ijknabla@gmail.com>
2023-08-30 11:39:12 +0900

    Implement municipality_by_name

--- gobo/database/municipality.py
+++ gobo/database/municipality.py
@@ -1,5 +1,21 @@
 from sqlite3 import Connection
 
+from ..types import MunicipalityID
+
 
 class Database:
     connection: Connection
+
+    def municipality_by_name(self, name: str) -> MunicipalityID:
+        cursor = self.connection.cursor()
+        cursor.execute(
+            """
+SELECT municipality_id
+FROM municipality_names
+WHERE `name` = ?
+        """,
+            (name,),
+        )
+
+        (id,) = map(MunicipalityID, cursor.fetchone())
+        return id
