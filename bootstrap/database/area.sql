347cd654e2f184513dc72985aa75814d5cf439f9
ijknabla <ijknabla@gmail.com>
2023-08-30 16:08:28 +0900

    Add area_names table

---
+++ bootstrap/database/area.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE area_names
+(
+    id int,
+    `name` text
+);
+INSERT INTO area_names
+(
+    id, `name`
+)
+VALUES
+    (1,'ベイエリア'),
+    (2,'東葛飾エリア'),
+    (3,'北総エリア'),
+    (4,'九十九里エリア'),
+    (5,'南房総エリア'),
+    (6,'かずさ・臨海エリア');
