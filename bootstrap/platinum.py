12edb6fa188ba660c9d6ff0741ec4b3888e85623
ijknabla <ijknabla@gmail.com>
2023-09-01 22:09:21 +0900

    Restore tqdm

--- bootstrap/platinum.py
+++ bootstrap/platinum.py
@@ -2,17 +2,17 @@ from __future__ import annotations
 
 import json
 import re
-from asyncio import Queue, gather, get_running_loop
-from collections.abc import Collection, Generator
+from asyncio import gather, get_running_loop
+from collections.abc import Collection, Generator, Iterable
 from concurrent.futures import ThreadPoolExecutor
 from itertools import product
-from operator import itemgetter
 from typing import TypedDict, cast
 
 from lxml import html
 from lxml.etree import _Element
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from tqdm import tqdm
 
 from .types import Spot
 
@@ -34,46 +34,33 @@ def find_boot_options(driver: WebDriver) -> Generator[BootOption, None, None]:
 
 
 async def get_spots(drivers: Collection[WebDriver], boot_option: BootOption) -> list[Spot]:
-    queue: Queue[StampRallySpot] = Queue()
-
-    for spot in sorted(boot_option["stampRallySpots"], key=itemgetter("spotId")):
-        await queue.put(spot)
+    iterator = iter(tqdm(boot_option["stampRallySpots"]))
 
     with ThreadPoolExecutor(len(drivers)) as executor:
         spots: list[Spot] = sum(
-            await gather(*(_each_get_spots(executor, driver, queue) for driver in drivers)), []
+            await gather(*(_each_get_spots(executor, driver, iterator) for driver in drivers)), []
         )
 
     return sorted(spots, key=lambda spot: spot["id"])
 
 
 async def _each_get_spots(
-    executor: ThreadPoolExecutor, driver: WebDriver, queue: Queue[StampRallySpot]
+    executor: ThreadPoolExecutor, driver: WebDriver, spots: Iterable[StampRallySpot]
 ) -> list[Spot]:
     loop = get_running_loop()
-    spots: list[Spot] = []
-
-    while not queue.empty():
-        source = await queue.get()
-        try:
-            spot = await loop.run_in_executor(executor, _get_spot, driver, source)
-            spots.append(spot)
-        finally:
-            queue.task_done()
-
-    return spots
+    return [await loop.run_in_executor(executor, _get_spot, driver, spot) for spot in spots]
 
 
-def _get_spot(driver: WebDriver, source: StampRallySpot) -> Spot:
-    spot = cast(
+def _get_spot(driver: WebDriver, spot: StampRallySpot) -> Spot:
+    result = cast(
         Spot,
         {
-            "id": source["spotId"],
-            "name": source["spotTitle"],
+            "id": spot["spotId"],
+            "name": spot["spotTitle"],
         },
     )
 
-    driver.get(f"https://platinumaps.jp/d/gogo-boso?s={spot['id']}")
+    driver.get(f"https://platinumaps.jp/d/gogo-boso?s={result['id']}")
     for frame in driver.find_elements(by=By.XPATH, value="//iframe"):
         driver.switch_to.frame(frame)
         for tr in driver.find_elements(by=By.XPATH, value='//tr[@class = "poiproperties__item"]'):
@@ -85,13 +72,13 @@ def _get_spot(driver: WebDriver, source: StampRallySpot) -> Spot:
             ):
                 match itemlabel.text:
                     case "住所":
-                        spot["address"] = a.text
+                        result["address"] = a.text
                     case "URL":
                         href = a.get_attribute("href")
                         assert href is not None
-                        spot["uri"] = href
+                        result["uri"] = href
 
-    return spot
+    return result
 
 
 def _find_boot_options_from_frame(document: _Element) -> Generator[BootOption, None, None]:
