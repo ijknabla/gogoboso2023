4f87cb009e3dddb7ca2c71add4a24ed5759af0fa
ijknabla <ijknabla@gmail.com>
2023-09-01 11:32:33 +0900

    Implement new database command

--- bootstrap/__main__.py
+++ bootstrap/__main__.py
@@ -6,7 +6,7 @@ from contextlib import AsyncExitStack, contextmanager
 from functools import wraps
 from pathlib import Path
 from sqlite3 import connect
-from typing import IO, Any, ParamSpec, TypeVar
+from typing import IO, Any, ParamSpec, TypeVar, cast
 
 import click
 from selenium import webdriver
@@ -44,11 +44,32 @@ def json_command(output: IO[str], indent: int | None) -> None:
 
 @main.command
 @run_decorator
+@click.option(
+    "-o", "--output", "output_file", type=click.File("w", encoding="utf-8"), default=sys.stdout
+)
+@click.argument("input_file", metavar="JSON", type=click.File("r", encoding="utf-8"))
+async def database(
+    input_file: IO[str],
+    output_file: IO[str],
+) -> None:
+    boot_option = cast(platinum.BootOption, json.load(input_file))
+
+    with connect(":memory:") as connection:
+        cursor = connection.cursor()
+        area.create_and_insert(cursor)
+        spot.create_and_insert(cursor, boot_option)
+
+        for sql in connection.iterdump():
+            print(sql, file=output_file)
+
+
+@main.command
+@run_decorator
 @click.option("-o", "--output", type=click.File("w", encoding="utf-8"), default=sys.stdout)
 @click.option(
     "--cache-path", type=click.Path(dir_okay=False, path_type=Path), default=Path(".cache.pickle")
 )
-async def database(output: IO[str], cache_path: Path) -> None:
+async def database_bak(output: IO[str], cache_path: Path) -> None:
     async with AsyncExitStack() as stack:
         enter = stack.enter_context
         connection = enter(connect(":memory:"))
